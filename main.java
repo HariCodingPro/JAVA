1b041479f2ff8ab031099b95ed0582a558fe9c0e
Ines12345678 <81796763+Ines12345678@users.noreply.github.com>
2023-04-18 14:55:07 +0200

    Add files via upload

---
+++ main.java
@@ -0,0 +1,22 @@
+package Test;
+
+import Forme.*;
+
+public class main {
+
+	public static void main(String[] args) {
+		Dechet d1, d2, d3, d4, d5, d6, d7, d8, d9, d10;
+		d1 = new Dechet("cartons", 0.4, "enveloppe");
+		d2 = new Dechet("cartons", 5, "emballage");
+		d3 = new Dechet("plastiques", 8, "bouteille");
+		d4 = new Dechet("plastiques", 1, "gobelet");
+		d5 = new Dechet("plastiques", 12, "yaourt");
+		d6 = new Dechet("verres", 14, "vodka");
+		d7 = new Dechet("verres", 6, "bière");
+		d8 = new Dechet("verres", 0.7, "moutarde");
+		d9 = new Dechet("métaux", 4.5, "cannette");
+		d10 = new Dechet("métaux", 9, "conserve");
+
+	}
+
+}
